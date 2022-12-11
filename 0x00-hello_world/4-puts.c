bb7457d0547baabf52dcad8e14910fa72f4fd4de
nejuka33 <nejatnure3@gmail.com>
2022-12-11 05:22:36 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - print a line of code using puts
+ *
+ * Return: o
+ */
+int main(void)
+{
+	pute("\"Programming is like building a multilingual puzzle");
+	return (o);
+}
