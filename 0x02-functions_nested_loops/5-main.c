1625278cd2c18e17ee5f8a2d916640997c032d41
nejuka33 <nejatnure3@gmail.com>
2022-12-14 05:57:58 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -9,22 +9,22 @@ int main(void)
 {
 	int r;
 
-	r = _print_sign(98);
+	r = print_sign(98);
 	_putchar(',');
 	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
-	r = _print_sign(0);
+	r = print_sign(0);
 	_putchar(',');
 	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
-	r = _print_sign(0xff);
+	r = print_sign(0xff);
 	_putchar(',');
 	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
-	r = _print_sign(-1);
+	r = print_sign(-1);
 	_putchar(',');
 	_putchar(' ');
 	_putchar(r + '0');
