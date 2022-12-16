b313a2f73b61745045f7b60e25b86540e80a3db8
nejuka33 <nejatnure3@gmail.com>
2022-12-16 04:37:03 -0800

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - printing line
+ * @n: number of lines
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
