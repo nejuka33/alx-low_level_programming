e3aa70b3ab863928c0caeac4ccb9d24d32467d50
nejuka33 <nejatnure3@gmail.com>
2022-12-23 10:06:53 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - lowercase letters of a string to upperca
+ * @n: pointer
+ * Return: n
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
